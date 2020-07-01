4a70eb3093ae752b3df55ed3c75fabc0650db83d
yumeidasari <32532981+yumeidasari@users.noreply.github.com>
2020-07-01 10:55:09 +0700

    Update welcome.blade.php

--- larashop/resources/views/welcome.blade.php
+++ larashop/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
         <title>Welcome</title>
     </head>
     <body>
-        <h1>SELAMAT DATANG!</h1>
+        <h1>SELAMAT DATANG {{$first_name}}!</h1>
         <h3> Terima kasih telah bergabung di Sanberbook. Social Media kita bersama!</h3>
     </body>
 </html>
